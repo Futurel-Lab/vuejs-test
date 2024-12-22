82aa217784cd2bf2797be0b5d244caaa68ef75ce
Payton <11670868+Futurel-Lab@users.noreply.github.com>
2024-12-22 12:07:44 -0600

    added games handling

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,47 @@ def greetings():
 def shark():
     return "🦈 Shark Attack 🦈"
 
+GAMES = [
+    {
+        'title': 'PomoFarm',
+        'genre': 'Focus',
+        'played': True,
+        'price': 4.99
+    },
+    {
+        'title': 'Call of Duty: Black Ops 6',
+        'genre': 'FPS',
+        'played': True,
+        'price': 69.99
+    },
+    {
+        'title': 'Minecraft',
+        'genre': 'SandBox',
+        'played': True,
+        'price': 26.99
+    },
+    {
+        'title': 'FrostPunk 2',
+        'genre': 'City Builder',
+        'played': False,
+        'price': 44.99
+    },
+    {
+        'title': 'Cities Skylines: 2',
+        'genre': 'City Builder',
+        'played': True,
+        'price': 59.99
+    }
+]
+
+# GET route handler
+@app.route('/games', methods=['GET'])
+def get_games():
+    return jsonify({
+        'games': GAMES,
+        'status': 'success'
+        })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
